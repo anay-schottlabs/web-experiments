dc51db65366bad6f1e44f409b2387bb851a2328c
anay-schottlabs <anay@schottlabs.com>
2025-11-05 23:08:53 -0800

    Changed styling

--- src/About.vue
+++ src/About.vue
@@ -1,6 +1,22 @@
 <script setup>
-// Component logic here
 </script>
 
 <template>
+  <div class="card card-body w-50 p-5 rounded-5">
+    <div class="text-white">
+      <h2 class="fs-1 pb-4 title">About Bad Bytes</h2>
+      <p class="fs-5">This page hasn't been finished yet!</p>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.card {
+  background-color: #090040;
+  border: 0.5em solid #FFCC00;
+}
+
+.btn {
+  color: #090040
+}
+</style>
--- src/Games.vue
+++ src/Games.vue
@@ -16,9 +16,9 @@ const imageDim = 250
     <div class="card w-50 p-3 rounded-5">
       <img class="card-image-top d-block mx-auto" :src="game.logo" :width="imageDim" :height="imageDim">
       <div class="card-body d-grid">
-        <h5 class="card-title text-white fs-2">{{ game.name }}</h5>
+        <h5 class="card-title fs-2 title">{{ game.name }}</h5>
         <p class="card-text text-white fs-4">{{ game.description }}</p>
-        <router-link :to="game.url" class="btn btn-primary fs-3 fw-bold btn-lg">Play</router-link>
+        <router-link :to="game.url" class="btn btn-primary fs-3 fw-bold title btn-lg">Play</router-link>
       </div>
     </div>
   </div>
@@ -29,7 +29,9 @@ const imageDim = 250
   background-color: #090040;
   border: 0.5em solid #FFCC00;
 }
-
+.card-title {
+  color: #FFCC00;
+}
 .btn {
   color: #090040
 }
