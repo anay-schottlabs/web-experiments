b065fe0dbc56814a643b8fdefb7527f90a80d2c5
Dandekar <anay.dandekar29@bcp.org>
2025-11-06 21:28:59 -0800

    Added colored icons

--- src/games/fireball/Fireball.vue
+++ src/games/fireball/Fireball.vue
@@ -1,7 +1,38 @@
 <script setup></script>
 
 <template>
-    THIS PAGE HAS NOT BEEN FINISHED YET
+    <div class="container text-white text-center flex">
+        <div class="row">
+            <div class="col">
+                Computer
+            </div>
+        </div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row"></div>
+        <div class="row">
+            <div class="col card ms-5">
+                Charge
+            </div>
+            <div class="col card mx-2">
+                Fireball
+            </div>
+            <div class="col card me-5">
+                Shield
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                You
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+    background-color: #090040;
+    color: white;
+    border: 0.25em solid #FFCC00;
+}
+</style>
