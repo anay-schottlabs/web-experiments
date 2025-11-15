b749929f782e94243ee09aa90a377a8c7e8e262a
anay-schottlabs <anay@schottlabs.com>
2025-11-14 23:30:59 -0800

    Changed navbar layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
+<script setup></script>
+
 <template>
   <div class="navbar bg-palette-blue p-5 shadow-xl sticky z-1 top-0">
-    <div class="flex-1">
-      <router-link to="/" class="btn btn-ghost text-3xl hover:bg-palette-black rounded-xl p-8">GameFridge</router-link>
+    <div class="navbar-start flex-1">
+      <router-link to="/" class="btn btn-ghost text-4xl hover:bg-palette-black rounded-xl p-8">GameFridge</router-link>
     </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
+    <div class="navbar-center content-center">
+      <ul class="menu menu-horizontal">
         <li>
-          <router-link to="/games" class="btn btn-ghost text-xl mx-3 hover:bg-palette-black">Games</router-link>
+          <router-link to="/games" class="btn btn-ghost text-2xl mx-3 p-5 py-6 rounded-xl hover:bg-palette-black">Games</router-link>
         </li>
         <li>
-          <router-link to="/about" class="btn btn-ghost text-xl mx-3 hover:bg-palette-black">About</router-link>
+          <router-link to="/about" class="btn btn-ghost text-2xl mx-3 p-5 py-6 rounded-xl hover:bg-palette-black">About</router-link>
         </li>
       </ul>
+    </div>
+    <div class="flex-1 navbar-end">
+      <div class="btn bg-palette-black text-white text-3xl p-7 hover:bg-palette-white
+      hover:text-palette-black border-3 shadow-none hover:italic">
+        1.0.0
+      </div>
     </div>
   </div>
   <div class="p-6">
