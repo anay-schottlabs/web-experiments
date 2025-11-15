c32fa99ab4c017c1d216e545bea7c6eeecb83c60
anay-schottlabs <anay@schottlabs.com>
2025-11-14 23:10:38 -0800

    Restyled about page

--- src/About.vue
+++ src/About.vue
@@ -1,30 +1,11 @@
-<script setup>
-</script>
-
 <template>
-  <div class="card card-body w-50 p-5 rounded-5">
-    <div class="text-white">
-      <h2 class="fs-1 pb-4 title">About Game Fridge</h2>
-      <p class="fs-5">
-        Game Fridge is a collection of small games and interactive experiments. Each one is designed to be easy to pick up and play—no installation, no hassle.
-      </p>
-      <p class="fs-5">
-        This site is always growing and changing as new games are added and old ideas get refreshed. Whether you want to spend a few minutes trying something different or explore new mechanics, you’ll find a variety of projects to check out here.
-      </p>
-      <p class="fs-5">
-        Feedback and suggestions are always welcome. Thanks for stopping by and enjoy exploring!
-      </p>
+  <div class="card bg-palette-white shadow-lg drop-shadow-[0_0px_10px_rgba(0,0,0,0.1)] hover:shadow-2xl">
+    <div class="card-body text-palette-black text-xl">
+      <div class="card-title text-3xl mb-3">About Game Fridge</div>
+      <p>This site is mostly little experiments and games I've made.</p>
+      <p>My design skills are pretty bad, so art won't be the best on this website!</p>
+      <p>Made this to test a bunch of stuff, and I'll add new things to do whenever I get ideas.</p>
+      <p>Expect new games every month or so.</p>
     </div>
   </div>
 </template>
-
-<style scoped>
-.card {
-  background-color: #090040;
-  border: 0.5em solid #FFCC00;
-}
-
-.btn {
-  color: #090040
-}
-</style>
