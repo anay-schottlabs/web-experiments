dfc7079d78627db877dc25b70f97c06852954a74
anay-schottlabs <anay@schottlabs.com>
2025-11-16 23:33:14 -0800

    Added favicon and logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
-<script setup></script>
+<script setup>
+import logo from "./assets/gamefridge_logo.png"
+</script>
 
 <template>
   <div class="navbar bg-palette-blue p-5 shadow-xl sticky z-1 top-0">
     <div class="navbar-start flex-1">
-      <router-link to="/" class="btn btn-ghost text-4xl hover:bg-palette-black rounded-xl p-8">GameFridge</router-link>
+      <img :src="logo" class="w-[5vw] h-[8vh] -mr-5">
+      <router-link to="/" class="btn btn-ghost text-4xl hover:bg-palette-black rounded-xl py-8">
+        <p class="text-4xl text-white">GameFridge</p>
+      </router-link>
     </div>
     <div class="navbar-center content-center">
       <ul class="menu menu-horizontal">
         <li>
-          <router-link to="/games" class="btn btn-ghost text-2xl mx-3 p-5 py-6 rounded-xl hover:bg-palette-black">Games</router-link>
+          <router-link to="/games" class="btn btn-ghost text-2xl mx-3 p-5 py-6 rounded-xl hover:bg-palette-black text-white">Games</router-link>
         </li>
         <li>
-          <router-link to="/about" class="btn btn-ghost text-2xl mx-3 p-5 py-6 rounded-xl hover:bg-palette-black">About</router-link>
+          <router-link to="/about" class="btn btn-ghost text-2xl mx-3 p-5 py-6 rounded-xl hover:bg-palette-black text-white">About</router-link>
         </li>
       </ul>
     </div>
